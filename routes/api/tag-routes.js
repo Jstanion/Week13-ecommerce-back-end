b52d8cec37d3c6d1c0df457871078577e43f7299
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-05-07 20:52:00 -0400

    created GET routes for tag data

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,39 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+
+    // find all tags and include its associated Product data
+    const tagData = await Tag.findAll({
+      include: [{model: Product}]
+    });
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+
+    // find a single tag by its `id`
+    const tagData = await Tag.findByPk(req.params.id, {
+      
+      // be sure to include its associated Product data
+      include: [{model: Product}]
+    });
+
+    if (!tagData) {
+      res.status(404).json({message: 'No product found with this id!'});
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
